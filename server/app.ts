4e29dbc1fac76c98d977507e2a179cc701ed7908
shahreaz0 <ashahreaz@gmail.com>
2024-10-06 03:53:27 +0600

    feat: implement expenses routes

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { rootRoute } from "./routes";
+import { HTTPException } from "hono/http-exception";
 
 export const app = new Hono();
 
@@ -7,4 +8,15 @@ app.route("/", rootRoute);
 
 app.get("/healthcheck", (c) => {
   return c.json({ message: "Ok", timestamp: Date.now() });
+});
+
+app.onError((error, c) => {
+  console.error(error.message);
+
+  if (error instanceof HTTPException && error.status) {
+    c.status(error.status);
+    return c.json({ message: error.message, status: error.status });
+  }
+
+  return c.json({ message: error.message, status: 500 });
 });
--- server/routes/expenses.route.ts
+++ server/routes/expenses.route.ts
@@ -1,19 +1,92 @@
 import { Hono } from "hono";
+import z from "zod";
+import { zValidator } from "@hono/zod-validator";
+import { HTTPException } from "hono/http-exception";
 
 export const expensesRoute = new Hono();
 
+const expenseSchema = z.object({
+  id: z.number().int().positive(),
+  title: z.string().min(3).max(100),
+  amount: z.number().positive(),
+});
+
+const expenseCreateSchema = expenseSchema.omit({ id: true });
+
+const expenseUpdateSchema = expenseSchema.partial().omit({ id: true });
+
+const fakeExpenses = [
+  {
+    id: 1,
+    title: "Groceries",
+    amount: 200.0,
+  },
+  {
+    id: 2,
+    title: "Car",
+    amount: 1000.0,
+  },
+  {
+    id: 3,
+    title: "Food",
+    amount: 500.0,
+  },
+];
+
 expensesRoute.get("/", (c) => {
-  return c.json({ type: "expenses get route" });
+  return c.json({ data: fakeExpenses });
 });
 
-expensesRoute.post("/", (c) => {
-  return c.json({ type: "expenses post route" });
+expensesRoute.get("/:id{[0-9]+}", (c) => {
+  if (!c.req.param("id")) {
+    throw new HTTPException(400, {
+      message: "Invalid ID",
+    });
+  }
+
+  return c.json({
+    message: "Success",
+    data: fakeExpenses.find((expense) => expense.id === +c.req.param("id")),
+  });
 });
 
-expensesRoute.put("/", (c) => {
-  return c.json({ type: "expenses put route" });
+expensesRoute.get("/total", (c) => {
+  return c.json({
+    message: "Success",
+    data: fakeExpenses.reduce((acc, cur) => acc + cur.amount, 0),
+  });
 });
 
-expensesRoute.delete("/", (c) => {
-  return c.json({ type: "expenses delete route" });
+expensesRoute.post("/", zValidator("json", expenseCreateSchema), (c) => {
+  const expenses = c.req.valid("json");
+
+  fakeExpenses.push({ id: fakeExpenses.length + 1, ...expenses });
+
+  c.status(201);
+  return c.json({ message: "Success", data: expenses });
+});
+
+expensesRoute.put("/:id{[0-9]+}", zValidator("json", expenseUpdateSchema), (c) => {
+  const id = c.req.param("id");
+
+  const expense = fakeExpenses.find((expense) => expense.id === +id);
+
+  return c.json({ message: "Success", data: expense || [] });
+});
+
+expensesRoute.delete("/:id{[0-9]+}", (c) => {
+  const id = c.req.param("id");
+
+  const expenseIndex = fakeExpenses.findIndex((expense) => expense.id === +id);
+  const expense = fakeExpenses.find((expense) => expense.id === +id);
+
+  if (expenseIndex === -1) {
+    throw new HTTPException(404, {
+      message: "Expense not found",
+    });
+  }
+
+  fakeExpenses.splice(expenseIndex, 1);
+
+  return c.json({ message: "Success", data: expense });
 });
